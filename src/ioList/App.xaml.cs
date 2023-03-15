8944fe986e763e4542ea44e8007032d2bdfb2eb7
Timothy Nunnink <46979634+tnunnink@users.noreply.github.com>
2023-03-15 15:21:24 -0500

    Adding release

--- src/ioList/App.xaml.cs
+++ src/ioList/App.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using Squirrel;
 
 namespace ioList
 {
@@ -7,6 +8,31 @@ namespace ioList
         public App()
         {
             InitializeComponent();
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            SquirrelAwareApp.HandleEvents(
+                onInitialInstall: OnAppInstall,
+                onAppUninstall: OnAppUninstall,
+                onEveryRun: OnAppRun);
+        }
+
+        private static void OnAppInstall(SemanticVersion version, IAppTools tools)
+        {
+            tools.CreateShortcutForThisExe(ShortcutLocation.StartMenu | ShortcutLocation.Desktop);
+        }
+
+        private static void OnAppUninstall(SemanticVersion version, IAppTools tools)
+        {
+            tools.RemoveShortcutForThisExe(ShortcutLocation.StartMenu | ShortcutLocation.Desktop);
+        }
+
+        private static void OnAppRun(SemanticVersion version, IAppTools tools, bool firstRun)
+        {
+            tools.SetProcessAppUserModelId();
+            // show a welcome message when the app is first installed
+            if (firstRun) MessageBox.Show("Thanks for installing my application!");
         }
     }
 }
